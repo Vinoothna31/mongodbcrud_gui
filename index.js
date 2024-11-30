5748cf7d4ea12390f4d39542a3be1f2bd5141b93
Vinoothna <vinoothnamukkamala@gmail.com>
2024-11-30 15:51:08 +0530

    connection string changed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app=express();
 const port=3000;
 
 //MongoDB connection
-mongoose.connect('mongodb://localhost:27017/branddb').then(
+mongoose.connect('mongodb+srv://VinoothnaMukkamala:vinoo31@cluster0.pwnro.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(
     ()=>console.log('Connected to MongoDB'))
     .catch(err=>console.error('Error connecting to MongoDb',err));
 
